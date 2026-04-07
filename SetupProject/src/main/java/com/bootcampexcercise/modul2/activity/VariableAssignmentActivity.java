ffd16264dec15e005d0ec4fe4095f96854a52aae
Seva280701 <seva280701@gmail.com>
2026-04-07 14:22:19 +0300

    Add VariableAssignmentActivity

---
+++ SetupProject/src/main/java/com/bootcampexcercise/modul2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+
+        int number = 0;
+        number = 5;
+
+        System.out.println(number);
+    }
+}
