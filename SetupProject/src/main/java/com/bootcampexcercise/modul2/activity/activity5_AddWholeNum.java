aa8c40e750719b3149558687224a42c8596d4c00
Seva280701 <seva280701@gmail.com>
2026-04-07 15:44:41 +0300

    Add AddWholeNum activity

---
+++ SetupProject/src/main/java/com/bootcampexcercise/modul2/activity/activity5_AddWholeNum.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class activity5_AddWholeNum {
+
+    public static void main(String[] args) {
+
+        // we want to find sum of numbers from 50 to 100
+        int sum = 0; // initialize sum variable
+
+        for (int i = 50; i <= 100; i++) { // loop from 50 to 100
+            sum = sum + i;
+        }
+
+        System.out.println("Sum of numbers from 50 to 100 is " + sum);
+    }
+}
