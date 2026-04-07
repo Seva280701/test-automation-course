1b67831c8942165bec62bba7a839d22acc4d48eb
Seva280701 <seva280701@gmail.com>
2026-04-07 15:00:10 +0300

    Add OperatorActivity class

---
+++ SetupProject/src/main/java/com/bootcampexcercise/modul2/activity/OperatorActivity.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class OperatorActivity {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        int sum = a + b;
+
+        System.out.println("Sum is: " + sum);
+    }
+}
